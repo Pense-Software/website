2403d0a20487208034fadb5e639975af07965cfc
Mar Pense <marcio@pense.dev>
2023-11-04 21:52:13 +0000

    Simplify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,33 +32,3 @@
 
 	<!-- <Counter /> -->
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: flex-start;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>

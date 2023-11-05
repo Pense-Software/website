0f1c8934f29ee72ae7454ddab832922d03301836
Mar Pense <marcio@pense.dev>
2023-11-05 08:10:23 +0000

    Add Heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { Skeleton, ImagePlaceholder, TextPlaceholder } from 'flowbite-svelte';
+	import { Heading, P, Button  } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
@@ -10,8 +11,11 @@
 	<meta name="description" content="Pense" />
 </svelte:head>
 
-<div class="pt-8">
-	<Skeleton class="mt-16 mb-8" />
-	<ImagePlaceholder class="my-8" />
-	<TextPlaceholder class="my-8" />
-</div>
+<div class="text-center">
+	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
+	<P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
+	<Button href="/">
+	  Learn more
+	  <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
+	</Button>
+  </div>

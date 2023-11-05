d1fb431c3dc7ba0fb1404535b7a62268b569f671
Mar Pense <marcio@pense.dev>
2023-11-05 07:16:43 +0000

    Add flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,25 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import "../app.postcss";
+  import Header from "./Header.svelte";
+  import "./styles.css";
 </script>
 
 <div class="app">
-	<Header/>
-	<main style="min-height: 70vh; padding: 50px;">
-		<slot />
-	</main>
+  <Header />
+  <main style="min-height: 70vh; padding: 50px;">
+    <slot />
+  </main>
 
-	<!-- Footer -->
-	<footer class="bg-white" style="text-align: center;">
-		<div class="container mx-auto px-6 py-4">
-		  <div class="flex justify-between items-center">
-			<p class="text-gray-700 text-sm">© 2023 Pense Lda.</p>
-			<div class="flex">
-			  <!-- Social Icons -->
-			  <!-- Add social media links here -->
-			</div>
-		  </div>
-		</div>
-	  </footer>
+  <!-- Footer -->
+  <footer class="bg-white" style="text-align: center;">
+    <div class="container mx-auto px-6 py-4">
+      <div class="flex justify-between items-center">
+        <p class="text-gray-700 text-sm">© 2023 Pense Lda.</p>
+        <div class="flex">
+          <!-- Social Icons -->
+          <!-- Add social media links here -->
+        </div>
+      </div>
+    </div>
+  </footer>
 </div>
-
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,114 +1,35 @@
 <script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, ImagePlaceholder, Skeleton, TextPlaceholder } from 'flowbite-svelte';
 	import { page } from '$app/stores';
 	import logo from '$lib/images/pense-logo-zoom.png';
 	import github from '$lib/images/github.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="/">
-			<img src={logo} alt="" />
-		</a>
-	</div>
-	<nav class="grow">
-		<ul>
-			<li>Home</li>
-			<li>About</li>
-		</ul>
-	</nav>
+	<div class="relative px-8">
+		<Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b">
+		  <NavBrand href="/">
+			<img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+		  </NavBrand>
+		  <NavHamburger />
+		  <NavUl>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/docs/components/navbar">Navbar</NavLi>
+			<NavLi href="/pricing">Pricing</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		  </NavUl>
+		</Navbar>
+		<div style="height:300px;" class="overflow-scroll pb-16">
+		  <Skeleton class="mt-16 mb-8" />
+		  <ImagePlaceholder class="my-8" />
+		  <TextPlaceholder class="my-8" />
+		</div>
+	  </div>
 	<div class="corner">
 		<a href="https://github.com/Pense-Software">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
 </header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: flex-end;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>

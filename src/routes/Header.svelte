b2288ae382451956837540d395d110e5fa13445e
Mar Pense <marcio@pense.dev>
2023-11-05 08:16:25 +0000

    Add margins

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@
 	import github from '$lib/images/github.svg';
 </script>
 
-<Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b">
+<Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b mb-4">
 	<NavBrand href="/">
 	<img src={logo} class="mr-3 h-6 sm:h-9" alt="Pense Logo" />
 	<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Pense</span>

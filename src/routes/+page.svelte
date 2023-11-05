cc693400879027fe9443444251abad0a6da7af40
Mar Pense <marcio@pense.dev>
2023-11-05 08:19:13 +0000

    Add margins

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	<meta name="description" content="Pense" />
 </svelte:head>
 
-<div class="text-center p-4">
+<div class="text-center p-4 mt-8">
 	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
 	<P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
 	<Button href="/">

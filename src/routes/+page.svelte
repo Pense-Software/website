e3810e41e85253dc1c44e710a35c71c12f5e0880
Mar Pense <marcio@pense.dev>
2023-11-05 09:06:59 +0000

    Fix align

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,15 @@
 
 <div class="text-center p-8 mt-16 md:width-2/3 min-h-screen">
 	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Building the new Internet</Heading>
-	<P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Pense builds digital products for well-known Web 3.0 brands <br>
+	<P class="mb-6 text-lg text-center lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Pense builds digital products for well-known Web 3.0 brands <br>
 	We team up with top developers and designers to make your ideas come to life</P>
 	<Button href="/">
 	  Learn more
 	  <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
 	</Button>
-	<h2>Clients</h2>
+	<Heading>Clients</Heading>
 	<Card img="https://variety.com/wp-content/uploads/2023/01/Twitter-Third-Party-Apps.png" href="https://icx.one" horizontal class="mb-4">
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ICX Social Network 2023/2024</h5>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">We are working together with ICX One to develop a social network similar to Twitter with many extra cool features.</p>
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ICX Social Network (2023/2024)</h5>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight text-left">We are working together with ICX One to develop a social network similar to Twitter with many extra cool features.</p>
 	</Card>
 </div>
